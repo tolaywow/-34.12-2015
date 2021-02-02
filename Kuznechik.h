0e652e093bfdb97dd735210c3f122d029b5a2786
tolaywow <61613891+tolaywow@users.noreply.github.com>
2021-02-02 21:16:15 +0300

    ver 0.1.3.1.1 add method on Kuznechik.h

---
+++ Kuznechik.h
@@ -0,0 +1,14 @@
+class GrassHopper
+{
+private:
+	void X(int8_t& blockKey, int8_t& blockText);
+	void S(int8_t& blockText);
+	void L(int8_t& blockText);
+	void R(int8_t& blockText);
+	int8_t l(int8_t& blockText);
+ 
+ public:
+	void in(int8_t& name_of_file_key,int8_t& name_of_file_text);
+	void out(int8_t& name_of_file_out);
+ 
+}
