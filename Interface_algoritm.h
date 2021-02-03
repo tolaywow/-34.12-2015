6893f6f8df5648b2787da13525be892ed95dc7ef
tolaywow <61613891+tolaywow@users.noreply.github.com>
2021-02-03 21:35:47 +0300

    ver 0.1.3.1.1.1 rebuild Interface

--- Interface_algoritm.h
+++ Interface_algoritm.h
@@ -1,7 +1,8 @@
 class Ishifr
 {
 public:
-	virtual void getKeyandBlock()=0;
+	virtual void getKey()=0;
+	virtual void getBlock()=0;
 	virtual void Work()=0;
 	virtual void outBlock()=0;
 	virtual ~Ishifr(){};
