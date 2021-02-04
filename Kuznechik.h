cc803f7083fbb09eb8c912c78f9e8e7716be8fff
tolaywow <61613891+tolaywow@users.noreply.github.com>
2021-02-04 23:34:16 +0300

    ver 0.1.3.1.2 add method on Kuznechik.cpp and .h

--- Kuznechik.h
+++ Kuznechik.h
@@ -1,14 +1,26 @@
-class GrassHopper
+#pragma once
+#include "Ikey.h"
+#include "Interface_algoritm.h"
+
+class GrassHopper: Ikey, Ishifr
 {
 private:
-	void X(int8_t& blockKey, int8_t& blockText);
-	void S(int8_t& blockText);
-	void L(int8_t& blockText);
-	void R(int8_t& blockText);
-	int8_t l(int8_t& blockText);
+	static const uint8_t length_of_key=0x20;
+	uint8_t* key;
+	void X(uint8_t& blockKey, uint8_t& blockText);
+	void S(uint8_t& blockText);
+	void L(uint8_t& blockText);
+	void R(uint8_t& blockText);
+	uint8_t l(uint8_t& blockText);
+	uint8_t pi(uint8_t& num);
  
  public:
-	void in(int8_t& name_of_file_key,int8_t& name_of_file_text);
-	void out(int8_t& name_of_file_out);
+	void getKey(uint8_t& key_length256_bit);
+	//void in(uint8_t& name_of_file_key, uint8_t& name_of_file_text);
+	//void out(uint8_t& name_of_file_out);
+	~GrassHopper()
+	{
+		delete[] key;
+	};
  
 }
